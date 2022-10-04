22fdd235035649e6f64e4449355b552a007665ae
seniyor-analyst <52724143+seniyor-analyst@users.noreply.github.com>
2022-10-04 17:11:01 -0400

    Update streamlit_app.py
    Updated the command for streamlit from print to text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Menu")
-streamlit.print("Omega 3 & Blueberry Oatmeal")
-streamlit.print("Kale, Spinach & Rocket Smoothie")
-streamlit.print("Hard-Boiled, Free-Range Egg")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard-Boiled, Free-Range Egg")
