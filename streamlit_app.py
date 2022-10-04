e6923813b56b2cf6745f0ecc51e87ec21e691d29
seniyor-analyst <52724143+seniyor-analyst@users.noreply.github.com>
2022-10-04 17:09:03 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,6 @@ import streamlit
 streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Menu")
-streamlit.out("Omega 3 & Blueberry Oatmeal")
+streamlit.print("Omega 3 & Blueberry Oatmeal")
+streamlit.print("Kale, Spinach & Rocket Smoothie")
+streamlit.print("Hard-Boiled, Free-Range Egg")
